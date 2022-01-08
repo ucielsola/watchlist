5b33be104084857aeecda70b97a06243c843794d
Uciel Sola <solauciel@gmail.com>
2022-01-08 14:11:30 -0300

    fixed missing imports

--- src/components/NotLoggedInBtns.js
+++ src/components/NotLoggedInBtns.js
@@ -1,14 +1,15 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import "./not-logged-in.css";
 import { ThemeSwitcher } from "./ThemeSwitcher";
+import { ThemeContext } from "./context/ThemeContext";
 
 export const NotLoggedInBtns = () => {
 	// Theme Switcher
 	const { darkTheme } = useContext(ThemeContext);
 
 	let darkClass = darkTheme ? " dark" : "";
-	
+
 	return (
 		<div className="nli__container">
 			<ThemeSwitcher />
