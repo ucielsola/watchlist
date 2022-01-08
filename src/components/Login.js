bc33369f2c3bd810f5a80ca800d3f81de9fa03cc
Uciel Sola <solauciel@gmail.com>
2022-01-08 14:21:56 -0300

    login msg

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,19 +1,19 @@
-import React, { useRef, useState, useContext } from 'react';
-import { Link, useHistory } from 'react-router-dom';
-import { useAuth } from '../context/AuthContext';
-import { ThemeContext } from '../context/ThemeContext';
+import React, { useRef, useState, useContext } from "react";
+import { Link, useHistory } from "react-router-dom";
+import { useAuth } from "../context/AuthContext";
+import { ThemeContext } from "../context/ThemeContext";
 
-import './login-signup-forgot-profile.css';
+import "./login-signup-forgot-profile.css";
 
 export const LogIn = () => {
 	// Theme Switcher
 	const { darkTheme } = useContext(ThemeContext);
-	let darkClass = darkTheme ? ' dark' : '';
+	let darkClass = darkTheme ? " dark" : "";
 	// Login
 	const emailRef = useRef();
 	const passwordRef = useRef();
 	const { login } = useAuth();
-	const [error, setError] = useState('');
+	const [error, setError] = useState("");
 	const [loading, setLoading] = useState(false);
 	const history = useHistory();
 
@@ -21,29 +21,37 @@ export const LogIn = () => {
 		e.preventDefault();
 
 		try {
-			setError('');
+			setError("");
 			setLoading(true);
 			await login(emailRef.current.value, passwordRef.current.value);
-			history.push('/');
+			history.push("/");
 		} catch (error) {
-			setError('Failed to log in');
+			setError("Failed to log in");
 		}
 		setLoading(false);
 	}
 
 	return (
-		<div className={'page__container' + darkClass}>
-			<div className={'page__wrapper' + darkClass}>
-				<h2 className={'page__title' + darkClass}>Log In</h2>
-				{error && <h3 className={'page__error-msg' + darkClass}>{error}</h3>}
+		<div className={"page__container" + darkClass}>
+			<div className={"page__wrapper" + darkClass}>
+				<h2 className={"page__title" + darkClass}>Log In</h2>
+				{error && <h3 className={"page__error-msg" + darkClass}>{error}</h3>}
 				<form action="" onSubmit={handleSubmit}>
-					<div className={'form__group' + darkClass}>
-						<input type="email" name="password" ref={emailRef} className={'form__input' + darkClass} />
-						<label htmlFor="email" className={'form__label' + darkClass} tabIndex="-1">
+					<div className={"form__group" + darkClass}>
+						<input
+							type="email"
+							name="password"
+							ref={emailRef}
+							className={"form__input" + darkClass}
+						/>
+						<label
+							htmlFor="email"
+							className={"form__label" + darkClass}
+							tabIndex="-1">
 							Email
 						</label>
 					</div>
-					<div className={'form__group' + darkClass}>
+					<div className={"form__group" + darkClass}>
 						<div className="forgot">
 							<Link to="/forgot-password" tabIndex="-1">
 								Forgot Password?
@@ -53,22 +61,33 @@ export const LogIn = () => {
 							type="password"
 							name="password"
 							ref={passwordRef}
-							className={'form__input' + darkClass}
+							className={"form__input" + darkClass}
 							minLength="6"
 						/>
-						<label htmlFor="password" className={'form__label' + darkClass} tabIndex="-1">
+						<label
+							htmlFor="password"
+							className={"form__label" + darkClass}
+							tabIndex="-1">
 							Password
 						</label>
 					</div>
-					<div className={'form__group' + darkClass}>
-						<button disabled={loading} type="submit" className={'form__submit' + darkClass}>
+					<div className={"form__group" + darkClass}>
+						<button
+							disabled={loading}
+							type="submit"
+							className={"form__submit" + darkClass}>
 							Log In
 						</button>
 					</div>
 				</form>
-				<div className={'to-signup' + darkClass}>
-					<p>You can use the test account 'test@test.com' (email) and 'watchlist' (password)</p>
-					Want your own account? <Link to="/signup">Sign Up!</Link>{' '}
+				<div className={"to-signup" + darkClass}>
+					<p>
+						You can use the test account 'test@test.com' (email) and 'watchlist'
+						(password)
+					</p>
+				</div>
+				<div className={"to-signup" + darkClass}>
+					Want your own account? <Link to="/signup">Sign Up!</Link>{" "}
 				</div>
 			</div>
 		</div>
